0bd57739b0b928e3bb0696925092f37076db66bf
blaureijs <34163574+blaureijs@users.noreply.github.com>
2019-05-23 15:41:53 -0300

    Remove old files from repository.

--- install_sentinelsat.bat
+++
@@ -1,7 +0,0 @@
-ECHO.
-ECHO -------------------------------------------------
-ECHO Installing "Sentinelsat" Python Library
-ECHO -------------------------------------------------
-ECHO.
-CD C:\python27\scripts
-.\pip install sentinelsat
--- install_sentinelsat_arcdir.bat
+++
@@ -1,7 +0,0 @@
-ECHO.
-ECHO -------------------------------------------------
-ECHO Installing "Sentinelsat" Python Library
-ECHO -------------------------------------------------
-ECHO.
-CD C:\python27\ArcGIS10.6\Scripts
-.\pip install sentinelsat
